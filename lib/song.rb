98d963de73fec84ff0b4520478a994dbd3c03cce
Andrew Eames <ajerxs@gmail.com>
2020-11-25 02:23:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,12 +34,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.find{|song| song.name == name} == nil
-      song = self.new
-      song.name = name
-      song
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
     else
-      self.all.find{|song| song.name == name}
+      self.find_by_name(name)
     end
   end
 
