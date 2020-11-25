bd6480751e4bc0cb9aa20c53d894711edf77f264
Andrew Eames <ajerxs@gmail.com>
2020-11-25 02:04:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,10 @@ class Song
     self.class.all << self
   end
 
-  def self.create
-    @@all << self.new
-    self
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
   end
 
 end
